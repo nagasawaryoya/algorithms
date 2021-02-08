dd14a3c34a6f10402cf24aab89bc6f2ab7328910
nagasawaryoya <nagasawaryoya@gmail.com>
2021-02-09 06:59:31 +0900

    バブルソート追加

---
+++ sort/bubble.ts
@@ -0,0 +1,30 @@
+/**
+ * 引数で受け取った配列をバブルソートして返却する。
+ *
+ * @param arr number[] ソートする配列
+ * @returns Generator<number | undefined, void, unknown>
+ */
+function *bubble(
+  arr: number[]
+): Generator<number | undefined, void, unknown> {
+  let count = 0;
+  let lastIndex = arr.length;
+
+  while (true) {
+    lastIndex = lastIndex - 1;
+    if (arr.length <= 0) break;
+    if (count === arr.length) {
+      yield arr.shift();
+      count = 0;
+      lastIndex = arr.length;
+    } else {
+      if (arr[lastIndex - 1] > arr[lastIndex]) {
+        const temp1 = arr[lastIndex - 1];
+        const temp2 = arr[lastIndex];
+        arr[lastIndex] = temp1;
+        arr[lastIndex - 1] = temp2;
+      }
+      count++;
+    }
+  }
+}
