595b991306a7f53f34a0d4f7623ac25805bd5011
nagasawaryoya <nagasawaryoya@gmail.com>
2021-02-10 07:32:14 +0900

    選択ソート追加

---
+++ sort/select.ts
@@ -0,0 +1,16 @@
+/**
+ * 引数で受け取った配列を選択ソートして返却する。
+ *
+ * @param arr number[] ソートする配列
+ * @returns Generator<number | undefined, void, unknown>
+ */
+function *select(
+  arr: number[]
+): Generator<number | undefined, void, unknown> {
+  while (arr.length > 0) {
+    yield arr.splice(
+        arr.findIndex(a => a === Math.min(...arr)),
+        1
+    )[0]
+  }
+}
