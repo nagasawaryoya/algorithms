3d1cbe0b46028f734ae201c578a1f55d180030e4
nagasawaryoya <nagasawaryoya@gmail.com>
2021-03-02 07:10:09 +0900

    [ マージソート ] 与えられた配列を最小まで分割し、その配列内で小さい順に並び替える

--- sort/merge.ts
+++ sort/merge.ts
@@ -6,4 +6,34 @@
  */
 const merge = (arr: number[]): number[] => {
   return arr;
-}
+};
+
+const test = [6, 4, 3, 7, 5, 1, 2];
+
+const split = (array: number[], result: any[] = []): any[] => {
+  const right = array.splice(Math.ceil(array.length / 2));
+  const left = array;
+
+  if (left.length === 1 && right.length === 1) {
+    result.push([left, right]);
+  } else if (left.length === 1) {
+    result.push([left]);
+  } else if (right.length === 1) {
+    result.push([right]);
+  }
+
+  if (left.length !== 1) {
+    split(left, result);
+  }
+  if (right.length !== 1) {
+    split(right, result);
+  }
+  return result;
+};
+
+const coalescence = (array: any[]) =>
+  array.map((arr) => (arr[0] < arr[1] || !arr[1] ? [arr[0], arr[1] ?? null] : [arr[1], arr[0]]));
+
+// const coalescence = (array: any[]) => array.map((arr) => arr);
+// split(test, []);
+console.log(coalescence(split(test)));
